2bdc8cc5cc2d85e50c279ebcc8e147fe35993aa4
Praveen Venkatesh <praveenv253@gmail.com>
2013-07-06 00:07:43 +0530

    Find mean and std deviation in unif. distribution.

--- pi-estimation/non-uniformity.py
+++ pi-estimation/non-uniformity.py
@@ -4,7 +4,15 @@ import scipy as sp
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    a = sp.random.random(1e8)
-    plt.hist(a, bins=100)
-    plt.show()
-
+    N = 1000
+    num_bins = 100
+    x = sp.zeros((N, num_bins))
+    for i in xrange(N):
+        print i
+        a = sp.random.random(1e7)
+        x[i] += plt.hist(a, bins=num_bins, normed=True)[0]
+    sp.save('non-uniformity-data-%s-bins.npy' % num_bins, x)
+    mu = sp.average(x, axis=0)
+    print mu
+    sigma = sp.std(x, axis=0)
+    print sigma
