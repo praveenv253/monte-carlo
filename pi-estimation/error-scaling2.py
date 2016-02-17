8a934f289b827c38ad37b2d8d860dd71afc66199
Praveen Venkatesh <praveenv253@gmail.com>
2016-02-17 01:27:44 -0500

    Make error-scaling2.py python3 compatible.

--- pi-estimation/error-scaling2.py
+++ pi-estimation/error-scaling2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 The real error scaling program.
@@ -6,12 +6,15 @@ Calculates standard deviation from the mean at each given total number of
 points. Then, we plot standard deviation against number of points.
 """
 
+from __future__ import print_function, division
+
 import sys
 import scipy as sp
 import matplotlib.pyplot as plt
 
+
 def find_pi_approx(num_points):
-    # Need to create a random 2D array and then find the number of points which 
+    # Need to create a random 2D array and then find the number of points which
     # lie inside the circular region
 
     # Create an array_dim x array_dim test array => num_points test points
@@ -25,14 +28,15 @@ def find_pi_approx(num_points):
     x = x*x + y*y
     x = sp.floor(x)
     # x now contains only 1's and 0's. The fraction of 0's corresponds to the
-    # region within the quarter-circle. 
+    # region within the quarter-circle.
     count = sp.dot(sp.ones([1, array_dim]), x)[0]
     count = num_points - count
 
-    # The value of pi is simply 4 * count / num_points^2, because we are 
+    # The value of pi is simply 4 * count / num_points^2, because we are
     # choosing a quarter circle, and num_points^2 is the total number of points
     result = count * 4 / num_points
     return num_points, result
+
 
 def get_error(num_points, num_iter=100):
     """
@@ -43,11 +47,12 @@ def get_error(num_points, num_iter=100):
     num_points = sp.floor(num_points)
 
     pi_values = []
-    for i in xrange(num_iter):
+    for i in range(num_iter):
         pi_values.append(find_pi_approx(num_points)[1])
 
     pi_values = sp.array(pi_values)
     return num_points, sp.std(pi_values)
+
 
 if __name__ == '__main__':
     points = sp.logspace(4, 6, 100)
@@ -55,11 +60,10 @@ if __name__ == '__main__':
     y = []
     for i in points:
         num_points, err = get_error(i)
-        print num_points, err
+        print(num_points, err)
         x.append(num_points)
         y.append(err)
 
     plt.figure(0)
     plt.plot(x, y)
     plt.show()
-
